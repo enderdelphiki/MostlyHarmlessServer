a1c85eba111707a2951ba29b3b5a9f5b5af9b4cf
Todd Beckman <toddbeckman1@gmail.com>
2014-12-15 13:08:20 -0700

    authlogs format

--- scripts.js
+++ scripts.js
@@ -7168,7 +7168,7 @@ init : function (){
     }
 
     AuthLogs.prototype.display = function(source, chan, num) {
-        var table = "<br><table><tr><th>User</th><th>Command</th><th>Target</th><th>Reason</th></tr>";
+        var table = "<style type='text/css'>th{border:solid black 1px;}td{border:solid black 1px;}</style><br><table><tr><th>User</th><th>Command</th><th>Target</th><th>Reason</th></tr>";
         for (var i = this.logs.length - 1; -1 < i && -1 < num; i--) {
             table += "<tr><td>" + this.logs[i][0] + "</td><td>" + this.logs[i][1] + "</td><td>" + this.logs[i][2] + "</td><td>" + this.logs[i][3] + "</td></tr>";
             num--;
