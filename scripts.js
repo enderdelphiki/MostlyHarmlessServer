0b92b0206347daeb6724dc1233d85ca44b3d78b8
Todd Beckman <toddbeckman1@gmail.com>
2015-01-10 00:33:50 -0700

    lots of stuff

--- scripts.js
+++ scripts.js
@@ -6486,8 +6486,7 @@ init : function (){
             name = "<b><font color='#D0A9F5'>[</font><font color='#D0A9F5'>H</font><font color='#9F81F7'>H</font><font color='#8258FA'>]</font><font color='#642EFE'>M</font><font color='#4000FF'>e</font><font color='#3A01DF'>s</font><font color='#3104B4'>s</font><font color='#29088A'>i</font><font color='#210B61'>a</font><font color='#170B3B'>h</font></b>";
         }
         else if (name == "[HH]Frost1076") {
-            str = str.substring(0, str.length - 1);
-            name = "<font color='#007399'>+</font><font color='#007EA8'>[</font><font color='#0086B3'>H</font><font color='#008EBD'>H</font><font color='#0099CC'>]</font><font color='#00A1D6'>F</font><font color='#00ACE6'>r</font><font color='#00BFFF'>o</font><font color='#1AC6FF'>s</font><font color='#33CCFF'>t</font><font color='#4DD2FF'>1</font><font color='#66D9FF'>0</font><font color='#80DFFF'>7</font><font color='#99e6ff'>6</font>";
+            name = "<font color='#007EA8'>[</font><font color='#0086B3'>H</font><font color='#008EBD'>H</font><font color='#0099CC'>]</font><font color='#00A1D6'>F</font><font color='#00ACE6'>r</font><font color='#00BFFF'>o</font><font color='#1AC6FF'>s</font><font color='#33CCFF'>t</font><font color='#4DD2FF'>1</font><font color='#66D9FF'>0</font><font color='#80DFFF'>7</font><font color='#99e6ff'>6</font>";
         }
         else if (name == "[HH]Jordan") {
             name = "<font color='#808080'>[</font><font color='purple'>H</font><font color='#808080'>H</font><font color='purple'>]</font><font color='#808080'>J</font><font color='purple'>o</font><font color='#808080'>r</font><font color='purple'>d</font><font color='#808080'>a</font><font color='purple'>n</font>";
